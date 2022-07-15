5ca3afb7353fc712f8d690352659c75a587273b3
Lukas-technoledge <chialukaonuoha@gmail.com>
2022-07-15 00:18:46 -0700

    updated work

--- src/components/AppInfo.js
+++ src/components/AppInfo.js
@@ -1,40 +1,84 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './AppInfo.css';
-import {useData} from '../hooks/useData';
+import { useData } from '../hooks/useData';
 import img from '../kindpng_490827.png'
+import Search from '../components/Search';
+import { FormControl, Select, MenuItem, InputLabel } from '@mui/material';
 
 function AppInfo() {
-    const {error, loading, data } = useData();
+    const { error, loading, data } = useData();
 
-    if(loading) return <div>Loading...</div>;
+    const [input, setInput] = React.useState('');
 
-    if(error) return <div>Something went wrong. Please check for error input...</div>
+    const [info, setInfo] = React.useState(data)
+    console.log('crazy', info)
+    useEffect(() => {
+    }, [])
 
-  return <div className="appInfo">
+    if (loading) return <div>Loading...</div>;
 
-    {data.launches.map(launch => {
-       return <div className="appInfo__info">
-        <h3 className="appInfo__date" >Date: { launch.launch_date_local }</h3>
-        <div className="appInfo__data">
-            <img className='image' src={img} alt="image"/>
-            <div className="appInfo__details"> <p> Mission name: { launch.mission_name}</p>
-                <p> Mission id: { launch.mission_id}</p>
-                <p> Rocket name: { launch.rocket.rocket_name}</p>
-                <p>Company name: { launch.rocket.rocket.company}</p>
-                <p>Site name: { launch.launch_site.site_name}</p>
+    if (error) return <div>Something went wrong. Please check for error input...</div>
+
+    const numbers = data.launches
+    console.log(numbers)
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+        if (input != '') {
+            let result = filterByName(input);
+
+            setInfo(result);
+            setInput('')
+        }
+        console.log('fiiiiiiisssh', input);
+    }
+
+    const filterByName = (name) => {
+        return data.launches.filter(item => item.rocket.rocket_name == name)
+    }
+    console.log('control', info)
+    console.log(data.launches)
+
+
+    return <div className="appInfo">
+
+        <div className='home'>
+            {/* search div */}
+            <Search />
+
+            {/* buttons */}
+            <div className="select">
+                <h2>{input}</h2>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Select</InputLabel>
+                    <Select onChange={(e) => handleChange(e) } value={input}>
+                        {numbers.map(launch => {
+                            return <MenuItem value={launch.rocket.rocket_name}>{launch.rocket.rocket_name}</MenuItem>
+                        })}
+                    </Select>
+                </FormControl>
             </div>
+
         </div>
-       </div>
-       
-    })}
+
+        {info?.map(launch => {
+            return <div className="appInfo__info">
+                <h3 className="appInfo__date" >Date: {launch.launch_date_local}</h3>
+                <div className="appInfo__data">
+                    <img className='image' src={img} alt="image" />
+                    <div className="appInfo__details"> <p> Mission name: {launch.mission_name}</p>
+                        <p> Mission id: {launch.mission_id}</p>
+                        <p> Rocket name: {launch.rocket.rocket_name}</p>
+                        <p>Company name: {launch.rocket.rocket.company}</p>
+                        <p>Site name: {launch.launch_site.site_name}</p>
+                    </div>
+                </div>
+            </div>
+
+        })}
+
 
     </div>
 }
 
 export default AppInfo
-
-{/*    <div className="appInfo__data">
-            <div className="appInfo__data--data">
-                <h2>{ launches.mission_name}</h2>
-            </div>
-        </div> */}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,21 @@ import { Button } from '@mui/material';
 import AppInfo from '../components/AppInfo';
 
 function Home() {
+
+
+  
   return (
     <div className='home'>
-      {/* search div */}
-      <Search />
+      {/* search div 
+      <Search />*/}
 
-      {/* buttons */}
+      {/* buttons 
       <div className="button">
       <Button variant='outlined'> Mission name </Button>
       <Button variant='outlined'> Rocket name </Button>
       <Button variant='outlined'> Company name </Button>
       <Button variant='outlined'> Site name </Button>
-      </div>
+      </div>*/}
 
       {/* date picture data */}
       <AppInfo />
